d3520cbe2623006f4266d01267ca55bec2f695b8
Andrew Holding <andy-git@evath.net>
2017-08-17 11:58:47 -0400

    Added code to download ENCODE data for figure

---
+++ ENCODE/000_download_encode.sh
@@ -0,0 +1,2 @@
+wget http://www.encodeproject.org/files/ENCFF591QUF/@@download/ENCFF591QUF.bam --no-check-certificate
+wget http://www.encodeproject.org/files/ENCFF783NXW/@@download/ENCFF783NXW.bam --no-check-certificate
