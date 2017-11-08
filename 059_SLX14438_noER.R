d80b8af00486c6c97f7c9ab25ed5183f3ba87aa1
Andrew Holding <andy-git@evath.net>
2017-11-08 13:43:57 +0000

    ER sites removed from CTCF control peaks

--- 059_SLX14438_noER.R
+++ 059_SLX14438_noER.R
@@ -6,15 +6,35 @@ jg.treatedCondition       =  "Fulvestrant"
 jg.untreatedCondition     =  "none"
 dbaSummits                <- 200
 
-filename<-"Rdata/059_SLX-14438_dba_human_ER_CTCF_noER.rda"
-if(!file.exists(filename)){
-    dbaExperiment <- jg.getDba(jg.experimentSampleSheet,dbaSummits)
-    dbaControl    <- jg.getDba(jg.controlSampleSheet,   dbaSummits)
-    save(dbaExperiment,dbaControl,file=filename)
-} else {
-    load(filename)
+#Fix as my computer is crashing on bParallel at the moment.
+jg.getDba.linear<-function (jg.experimentSampleSheet,dbaSummits, ...)
+{
+    
+    dba <- dba(sampleSheet = jg.experimentSampleSheet)
+    if(exists("dbaSummits"))
+    {
+        dba <- dba.count(dba, summits=dbaSummits, ...)
+    } else {
+        dba <- dba.count(dba)
+    }
+    dba <- dba.count(dba, peaks=NULL, score=DBA_SCORE_READS)
+    return(dba)
 }
 
+#Load data used in the paper.
+filename<-"Rdata/040_SLX-14438_dba_human_ER_CTCF.rda"
+load(filename)
+
+#filename<-"Rdata/059_SLX-14438_dba_human_ER_CTCF_noER.rda"
+#if(!file.exists(filename)){
+#    dbaExperiment <- jg.getDba(jg.experimentSampleSheet,dbaSummits)
+#    dbaControl    <- jg.getDba(jg.controlSampleSheet,   dbaSummits, minOverlap=5)
+#    save(dbaExperiment,dbaControl,file=filename)
+##} else {
+#    load(filename)
+#}
+
+#dbaControl    <- jg.getDba.linear(jg.controlSampleSheet,  dbaSummits, minOverlap=5)
 
 #Load Sample Ids from control sample sheet.
 jg.sampleIds <- jg.getSampleIds(jg.controlSampleSheet)
@@ -43,6 +63,7 @@ jg.coefficient<-jg.getNormalizationCoefficient(jg.controlCountsTreated,
 
 #Check by MA plot (Optional)
 jg.plotMA(jg.experimentPeakset,jg.controlPeakset,jg.untreatedNames,jg.treatedNames,jg.coefficient)
+jg.plotMA(jg.experimentPeakset,jg.controlPeakset,jg.untreatedNames,jg.treatedNames,1)
 
 #DeSeq called by Diffbind will divide though by library size to normalise, 
 #and therefore partially undo our work. Solution is to correct our normalisation
@@ -76,5 +97,112 @@ dba.plotMA(jg.dba_analysis,bFlip=TRUE)
            
 #Check CTCF 
 ctcf.dba.analysis<-dba.analyze(dbaControl)
- dba.plotMA(ctcf.dba.analysis,bFlip=TRUE)          
- 
+ dba.plotMA(ctcf.dba.analysis,bFlip=TRUE) 
+
+#Run on DeSeq
+jg.controlPeakset    <- jg.dbaGetPeakset(dba.count(dbaControl, peaks=NULL, score=DBA_SCORE_READS))
+jg.controlPeaksetDeSeq<-jg.convertPeakset(jg.controlPeakset )
+jg.controlSizeFactors = estimateSizeFactorsForMatrix(jg.controlPeaksetDeSeq)
+
+jg.conditions <- read.csv(file=jg.controlSampleSheet, header=TRUE, sep=",")['Condition']
+
+jg.controlDeSeq<-jg.runDeSeq(jg.controlPeaksetDeSeq, jg.conditions,jg.controlSizeFactors)
+jg.controlResultsDeseq   = results(jg.controlDeSeq)
+
+jg.plotDeSeq(jg.controlResultsDeseq,
+             p=0.01, 
+             title.main="Fold-change in CTCF binding",
+             flip=T
+)
+
+
+###################
+#Load in the samples with out ER peaks
+#dbaControl_noER  <- jg.getDba.linear(jg.controlSampleSheet, bRemoveDuplicates=TRUE)
+
+filename<-"Rdata/059_SLX-14438_dba_human_ER_CTCF_noER.rda"
+#save(dbaControl_noER,file=filename)
+load(filename)
+## Repeat above analysis
+
+jg.controlPeakset    <- jg.dbaGetPeakset(dbaControl_noER)
+
+
+#Get counts for each condition
+jg.controlCountsTreated<-jg.getControlCounts(jg.controlPeakset, 
+                                             jg.controlSampleSheet,
+                                             jg.treatedCondition)
+jg.controlCountsUntreated<-jg.getControlCounts(jg.controlPeakset,
+                                               jg.controlSampleSheet,
+                                               jg.untreatedCondition)
+
+#Get sample names for conditions
+jg.untreatedNames <- names(jg.controlCountsUntreated)
+jg.treatedNames   <- names(jg.controlCountsTreated)
+
+##Get Normalization Coefficient
+jg.coefficient<-jg.getNormalizationCoefficient(jg.controlCountsTreated,
+                                               jg.controlCountsUntreated)
+
+
+#Check by MA plot (Optional)
+jg.plotMA(jg.experimentPeakset,jg.controlPeakset,jg.untreatedNames,jg.treatedNames,jg.coefficient)
+jg.plotMA(jg.experimentPeakset,jg.controlPeakset,jg.untreatedNames,jg.treatedNames,1)
+
+#DeSeq called by Diffbind will divide though by library size to normalise, 
+#and therefore partially undo our work. Solution is to correct our normalisation
+#factor to with this correction factor to remove library size part of our 
+#NormalisationFactor.
+
+jg.correctionFactor<-jg.getCorrectionFactor(jg.experimentSampleSheet,
+                                            jg.treatedNames,
+                                            jg.untreatedNames
+)
+
+
+#Apply coefficent and control factor
+jg.experimentPeaksetNormalised<-jg.applyNormalisation(jg.experimentPeakset,
+                                                      jg.coefficient,
+                                                      jg.correctionFactor,
+                                                      jg.treatedNames
+)
+
+
+#Return values to Diffbind and plot normalised result.
+jg.dba <- DiffBind:::pv.resetCounts(dbaExperiment,
+                                    jg.experimentPeaksetNormalised
+)
+
+
+#Analyze and plot with Diffbind      
+jg.dba_analysis<-dba.analyze(jg.dba)
+png("plots/059_CTCF_noER_MAplot.png",pointsize=15)
+dba.plotMA(jg.dba_analysis,bFlip=TRUE)
+dev.off()
+#Check CTCF 
+ctcf.dba.analysis<-dba.analyze(dbaControl_noER)
+
+dba.plotMA(ctcf.dba.analysis,bFlip=TRUE) 
+
+
+#Run on DeSeq
+jg.controlPeakset    <- jg.dbaGetPeakset(dba.count(dbaControl_noER, peaks=NULL, score=DBA_SCORE_READS))
+jg.controlPeaksetDeSeq<-jg.convertPeakset(jg.controlPeakset )
+jg.controlSizeFactors = estimateSizeFactorsForMatrix(jg.controlPeaksetDeSeq)
+
+jg.conditions <- read.csv(file=jg.controlSampleSheet, header=TRUE, sep=",")['Condition']
+
+jg.controlDeSeq<-jg.runDeSeq(jg.controlPeaksetDeSeq, jg.conditions,jg.controlSizeFactors)
+jg.controlResultsDeseq   = results(jg.controlDeSeq)
+
+png("plots/059_CTCF_noER_control_MAplot.png",pointsize=15)
+jg.plotDeSeq(jg.controlResultsDeseq,
+             p=0.01, 
+             title.main="Fold-change in CTCF binding",
+             flip=T
+)
+dev.off()
+
+png("plots/059_CTCF_noER_Normalisation_plot.png",pointsize=15)
+jg.plotNormalization(jg.controlCountsTreated,jg.controlCountsUntreated)
+dev.off()
